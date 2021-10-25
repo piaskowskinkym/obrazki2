79a9fd27f5a26f7d43a388170920986550657f73
Michał Piaskowski <M.Piaskowski@student.zschie.edu.pl>
2021-10-25 23:42:42 +0200

    Koniec commit z domu z nowego repozytorium

---
+++ app/src/main/java/com/example/galeria/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.galeria
+
+import android.R.attr
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
+import java.util.jar.Manifest
+import kotlinx.android.synthetic.main.activity_main.*
+import android.graphics.Matrix;
+
+import android.R.attr.pivotY
+
+import android.R.attr.pivotX
+
+import android.R.attr.angle
+import android.app.ActionBar
+import android.widget.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var ukryj = findViewById<CheckBox>(R.id.checkBox);
+        var obrazek = findViewById<ImageView>(R.id.imageView)
+        var ibPrawo = findViewById<ImageButton>(R.id.ibPrawo)
+        var ibLewo = findViewById<ImageButton>(R.id.ibLewo)
+        var ibKamera = findViewById<ImageButton>(R.id.ibKamera)
+        var edRotacja = findViewById<EditText>(R.id.rotacja)
+        var edObrot = findViewById<EditText>(R.id.obrot)
+        var bedytuj = findViewById<Button>(R.id.bedytuj)
+        var licznik = 1
+        ibKamera.isEnabled = false
+
+
+        //sprawdzanie czy mamy pozwolenie na kamere
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 111)
+        }else
+            ibKamera.isEnabled = true
+
+            ibKamera.setOnClickListener {
+                var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(i, 101)
+            }
+
+       bedytuj.setOnClickListener {
+           val nowys: String = edRotacja.getText().toString()
+           val nowyi = nowys.toInt()
+           val nowys2: String = edObrot.getText().toString()
+           val nowyi2 = nowys2.toFloat()
+
+           obrazek.requestLayout()
+           obrazek.getLayoutParams().height = nowyi
+           obrazek.getLayoutParams().width = nowyi
+           obrazek.setRotation(nowyi2)
+
+       }
+
+
+
+
+        //ukrywanie obrazków
+        ukryj.setOnClickListener {
+            if(ukryj.isChecked()){
+                obrazek.setVisibility(View.INVISIBLE)
+            }else{
+                obrazek.setVisibility(View.VISIBLE)
+            }
+        }
+        //ruch w prawo
+        ibPrawo.setOnClickListener{
+            licznik++
+            when(licznik) {
+                1 -> obrazek.setImageResource(R.drawable.ev1)
+                2 -> obrazek.setImageResource(R.drawable.ev2)
+                3 -> obrazek.setImageResource(R.drawable.f)
+                4 -> obrazek.setImageResource(R.drawable.exo)
+
+            }
+        }
+        //ruch w lewo
+       ibLewo.setOnClickListener{
+            licznik--
+            when(licznik) {
+                1 -> obrazek.setImageResource(R.drawable.ev1)
+                2 -> obrazek.setImageResource(R.drawable.ev2)
+                3 -> obrazek.setImageResource(R.drawable.f)
+                4 -> obrazek.setImageResource(R.drawable.exo)
+            }
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==101)
+        {
+
+            var zdjecie: Bitmap?
+            zdjecie = data?.getParcelableExtra("data")
+            findViewById<ImageView>(R.id.imageView).setImageBitmap(zdjecie)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==111&&grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            setContentView(R.layout.activity_main)
+            ibKamera.isEnabled = true
+        }
+    }
+
+}
